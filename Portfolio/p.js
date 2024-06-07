adab6f922d5d67991da7f452f71365129b7ed5e1
SAKSHI GAIKWAD <86105184+SakshiGaikwad@users.noreply.github.com>
2024-06-08 02:24:33 +0530

    Add files via upload

---
+++ Portfolio/p.js
@@ -0,0 +1,20 @@
+//This code is for scrolling the page upwards i.e. at the top of the webpage
+
+let mybutton = document.getElementById("scrollToTop");
+
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
